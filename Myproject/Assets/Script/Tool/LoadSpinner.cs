82d3236ce5d11b36d5497676f3152307bc80f278
DESKTOP-93GFS8D\EodlsRHD <eodls0810@gmail.com>
2024-10-12 19:00:21 +0900

    add loading spinner code

---
+++ Myproject/Assets/Script/Tool/LoadSpinner.cs
@@ -0,0 +1,39 @@
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSpinner : MonoBehaviour
+{
+    [SerializeField] private Image _loadingBar = null;
+    [SerializeField] private Transform _loadingBarTransform = null;
+
+    private float LoadingDuration = 3f;
+    private float rotateDuration = 1.5f;
+
+    public void Initialize()
+    {
+        RotateLoading();
+
+        StopLoading();
+    }
+
+    public void StartLoading()
+    {
+        _loadingBar.DOPlay();
+        _loadingBarTransform.DOPlay();
+    }
+
+    public void StopLoading()
+    {
+        _loadingBar.DOPause();
+        _loadingBarTransform.DOPause();
+    }
+
+    private void RotateLoading()
+    {
+        _loadingBar.fillAmount = 0f;
+        _loadingBar.DOFillAmount(1f, LoadingDuration).SetEase(DG.Tweening.Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+        _loadingBarTransform.DORotate(new Vector3(0f, 0f, -360f), rotateDuration, RotateMode.FastBeyond360).SetEase(DG.Tweening.Ease.Linear).SetLoops(-1);
+    }
+}
